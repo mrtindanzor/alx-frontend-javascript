ebde629cdfa02c22107316eabc3f9e0a279f6fa3
Mr. Tindanzor <ktindanzor@gmail.com>
2025-11-05 21:18:00 +0000

    remove declare from namespace

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,9 +1,5 @@
 import { RowElement, RowID } from "./interface";
 
-declare namespace CRUD {
-  function insertRow(row: RowElement): number
-  function deleteRow(rowId: RowID): void
-  function updateRow(rowId: RowID, row: RowElement): RowID
-}
-
-export = CRUD
+declare function insertRow(row: RowElement): number
+declare function deleteRow(rowId: RowID): void
+declare function updateRow(rowId: RowID, row: RowElement): RowID
